c3be566e21ab9f283cd8d0e7dcfada9784340535
songyinghui <song_xiaohui@outlook.com>
2018-08-14 13:09:50 +0800

    mapper

--- src/main/java/com/third/easyprice/controller/TestController.java
+++ src/main/java/com/third/easyprice/controller/TestController.java
@@ -1,16 +1,30 @@
 package com.third.easyprice.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.third.easyprice.dao.ShopDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/price")
 public class TestController {
 
+    @Autowired
+    private ShopDao shopDao ;
+
     @RequestMapping(value = "/test" , method = RequestMethod.GET)
-    public String test(){
-        return "success";
+    public JSONObject test(){
+
+        String name = "1" ;
+        List<Object> objects = shopDao.queryByName(name);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("data", objects) ;
+        jsonObject.put("msg" , "success") ;
+        return jsonObject;
 
     }
 }
