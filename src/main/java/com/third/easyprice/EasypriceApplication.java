0c4354c67b6b202e5201b85d1da524f5214681a3
songyinghui <song_xiaohui@outlook.com>
2018-08-22 16:29:04 +0800

    测试ssh

--- src/main/java/com/third/easyprice/EasypriceApplication.java
+++ src/main/java/com/third/easyprice/EasypriceApplication.java
@@ -9,4 +9,7 @@ public class EasypriceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EasypriceApplication.class, args);
     }
+	public void yesterday(){
+		
+	}
 }
